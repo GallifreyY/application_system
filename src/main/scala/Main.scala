352a797735e6fbd834a6c857e3cea5133fba8b8e
Ren-Junjie <E0657007@u.nus.edu>
2022-03-06 15:32:47 +0800

    completed CRUD functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,9 @@
 import java.awt.Choice
 import java.sql.ResultSet
-import cats.effect._
+import cats.effect.*
 import cats.syntax.all.*
+
+import java.util.Date
 import io.StdIn.*
 
 object Main extends IOApp {
@@ -22,40 +24,76 @@ object Main extends IOApp {
     sql_statement.executeUpdate("CREATE TABLE IF NOT EXISTS employment(applicant_id CHAR(9) NOT NULL, "+
     "occupation VARCHAR(64), employed_from DATE, employed_to DATE, company_name VARCHAR(64) NOT NULL, "+
     "PRIMARY KEY(applicant_id), FOREIGN KEY(applicant_id) REFERENCES applicants(applicant_id));")
-    sql_statement.executeUpdate("CREATE TABLE IF NOT EXISTS language(applicant_id CHAR(9) NOT NULL, type VARCHAR(64), "+
+    sql_statement.executeUpdate("CREATE TABLE IF NOT EXISTS language(applicant_id CHAR(9) NOT NULL, exam_type VARCHAR(64), "+
     "exam_date DATE, score INT, listening INT, reading INT, speaking INT, writing INT, PRIMARY KEY(applicant_id), "+
     "FOREIGN KEY (applicant_id) REFERENCES applicants(applicant_id));")
     }
 
-  private def insert(): Unit = {
+  private def insert(applicant_id: Int, name:String, gender: String, birthday: String, email: String,
+                     university_name:String, location:String, qualification:String, major:String,
+                     start_date:String, end_date:String, best_score: Int, gpa: Int, ranking: Int, sponsorship: Int,
+                     occupation:String, employed_from:String, employed_to: String, company_name:String,
+                     exam_type: String,exam_date: String, score: Int, listening: Int, reading: Int, speaking: Int, writing: Int): Unit = {
     //println("Start Initializing the Database...")
     val sql_statement = connection.createStatement()
-    sql_statement.executeUpdate("INSERT INTO applicants VALUES (000000005, 'name', 'gender', '2022-03-05', 'email');")
+    sql_statement.executeUpdate(s"INSERT INTO applicants VALUES (${applicant_id}, '${name}', '${gender}', STR_TO_DATE('${birthday}','%Y-%m-%d'), '${email}');")
+    sql_statement.executeUpdate(s"INSERT INTO education VALUES (${applicant_id}, '${university_name}', '${location}', '${qualification}', '${major}'," +
+    s"STR_TO_DATE('${start_date}','%Y-%m-%d'), STR_TO_DATE('${end_date}','%Y-%m-%d'),${best_score},${gpa}, ${ranking},${sponsorship});")
+    sql_statement.executeUpdate(s"INSERT INTO employment VALUES (${applicant_id}, '${occupation}', STR_TO_DATE('${employed_from}','%Y-%m-%d'), STR_TO_DATE('${employed_to}','%Y-%m-%d'),'${company_name}');")
+    sql_statement.executeUpdate(s"INSERT INTO language VALUES (${applicant_id}, '${exam_type}', STR_TO_DATE('${exam_date}','%Y-%m-%d'), ${score},${listening}, ${reading},${speaking},${writing});")
+  }
+//STR_TO_DATE("${birthday}","%Y-%m-%d")
+  private def delete(applicant_id: Int): Unit = {
+    val sql_statement = connection.createStatement()
+    sql_statement.executeUpdate(s"DELETE FROM education WHERE applicant_id = ${applicant_id};")
+    sql_statement.executeUpdate(s"DELETE FROM employment WHERE applicant_id = ${applicant_id};")
+    sql_statement.executeUpdate(s"DELETE FROM language WHERE applicant_id = ${applicant_id};")
+    sql_statement.executeUpdate(s"DELETE FROM applicants WHERE applicant_id = ${applicant_id};")
   }
 
-  private def delete(): Unit = {
+  private def update(applicant_id: Int,table: String, column: String, new_value: Any): Unit = {
     //println("Start Initializing the Database...")
     val sql_statement = connection.createStatement()
-    sql_statement.executeUpdate("DELETE FROM applicants WHERE applicant_id = 1;")
+    new_value match {
+      case new_value: String => sql_statement.executeUpdate(s"UPDATE ${table} SET ${column} = '${new_value}' WHERE applicant_id = ${applicant_id};")
+      case new_value: Int => sql_statement.executeUpdate(s"UPDATE ${table} SET ${column} = ${new_value} WHERE applicant_id = ${applicant_id};")
+      case _ => println("Please input either a string or an int.")
+    }
   }
 
-  private def update(): Unit = {
-    //println("Start Initializing the Database...")
-    val sql_statement = connection.createStatement()
-    sql_statement.executeUpdate("UPDATE applicants SET name = 'namee' WHERE applicant_id = 2;")
-  }
 
-  private def view(): Unit = {
+  private def view(applicant_id: Int,table:String): Unit = {
     //println("Start Initializing the Database...")
     val sql_statement = connection.createStatement()
-    sql_statement.executeUpdate("UPDATE applicants SET name = 'namee' WHERE applicant_id = 2;")
+    sql_statement.executeQuery(s"SELECT * from applicants WHERE applicant_id = ${applicant_id};")
+    println(s"'${table}' info of applicant_id: ${applicant_id} is: ")
+    import java.sql.ResultSet
+    val res:ResultSet = sql_statement.executeQuery(s"SELECT * from applicants WHERE applicant_id = ${applicant_id};")
+    val resmd = res.getMetaData
+    val colNum = resmd.getColumnCount
+    while (res.next) {
+      for (i <- 1 to colNum) {
+        if (i > 1) {
+          val colVal = res.getString(i)
+          println(resmd.getColumnName(i) + ": " + colVal + ",")
+        }
+      }
+      //println("")
+    }
   }
 
   // main
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      _ <- IO(println("Welcome to the applicantion system.\n"))
+      _ <- IO(println("Welcome to the application system.\n"))
       _ <- IO(iniDB())
-      _ <- IO(update())
+      //val id = List(1,2,3,4,5)
+      //_ <- IO(insert(2,"name","gender", "2022-03-05","email",
+      //  "university_name", "location", "qualification", "major", "2022-03-01", "2022-03-05", 90,5,5,1,
+      //  "occupation", "2022-01-01", "2022-02-01", "company_name",
+      //  "IELTS","2020-03-01",7,8,7,8,7))
+      //_ <- IO(delete(1))
+      //_ <- IO(update(2,"education", "gpa", 4.0))
+      _ <- IO(view(2,"education"))
     } yield ExitCode.Success
 }
